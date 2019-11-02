77759c4acb8b422250bc074ab95b2b9bcc829582
V. Buko <butkovv1991@gmail.com>
2019-11-02 17:21:14 +0300

    remove brain-even.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from '..';
+import brainGame from '../brainlib';
 
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
+brainGame();
