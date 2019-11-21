9fbbbb6932ee7f6b5dc5a120361095bfc8bbe065
V. Buko <butkovv1991@gmail.com>
2019-11-21 21:47:45 +0300

    move gameRound function outside of gamePrime function

--- src/games/primelib.js
+++ src/games/primelib.js
@@ -3,34 +3,34 @@ import {
   displayIntro, displayQuestion, checkAnswer, displayEnding,
 } from '../brainlib';
 
+const gameRound = () => {
+  const upperLimit = 10000; // Задаем максимальное значение генерируемого числа
+  let n = makeRandom(upperLimit); // Генерируем случайное число в заданных пределах
+  const divisors = [];
+  const uniqueDivisors = [];
+  let f = 2; // Начальное значение делителя для алгоритма поиска
+
+  displayQuestion(n);
+
+  // Реализуем алгоритм поиска делителей числа n с добавлением их в массив divisors
+  while (n > 1) {
+    if (n % f === 0) {
+      divisors.push(f);
+      n /= f;
+    } else f += 1;
+  }
+  // Заполняем массив uniqueDivisors уникальными значениями из массива divisors
+  for (let i = 0; i < divisors.length; i += 1) {
+    if (divisors[i] !== divisors[i - 1]) uniqueDivisors.push(divisors[i]);
+  }
+
+  const isPrime = uniqueDivisors.length === 1 ? 'yes' : 'no';
+
+  if (checkAnswer(isPrime) === true) gameRound();
+};
+
 const gamePrime = () => {
   const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-
-  const gameRound = () => {
-    const upperLimit = 10000; // Задаем максимальное значение генерируемого числа
-    let n = makeRandom(upperLimit); // Генерируем случайное число в заданных пределах
-    const divisors = [];
-    const uniqueDivisors = [];
-    let f = 2; // Начальное значение делителя для алгоритма поиска
-
-    displayQuestion(n);
-
-    // Реализуем алгоритм поиска делителей числа n с добавлением их в массив divisors
-    while (n > 1) {
-      if (n % f === 0) {
-        divisors.push(f);
-        n /= f;
-      } else f += 1;
-    }
-    // Заполняем массив uniqueDivisors уникальными значениями из массива divisors
-    for (let i = 0; i < divisors.length; i += 1) {
-      if (divisors[i] !== divisors[i - 1]) uniqueDivisors.push(divisors[i]);
-    }
-
-    const isPrime = uniqueDivisors.length === 1 ? 'yes' : 'no';
-
-    if (checkAnswer(isPrime) === true) gameRound();
-  };
 
   displayIntro(description);
 
