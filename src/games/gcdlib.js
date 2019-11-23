ace875c4adbc542d4b714b3a8ce7e2951bf20cf2
V. Buko <butkovv1991@gmail.com>
2019-11-23 14:45:35 +0300

    move gameRound function outside of gameGCD function

--- src/games/gcdlib.js
+++ src/games/gcdlib.js
@@ -3,22 +3,22 @@ import {
   displayIntro, displayQuestion, checkAnswer, displayEnding,
 } from '../brainlib';
 
+const gameRound = () => {
+  const a = makeRandom();
+  const b = makeRandom();
+  const question = `${a} ${b}`;
+  // Находим число, делящее a и b без остатка.
+  // Перебираем значения от наименьшего из a и b до 1.
+  let i = a > b ? b : a;
+  while (a % i !== 0 || b % i !== 0) i -= 1;
+
+  displayQuestion(question);
+
+  if (checkAnswer(String(i)) === true) gameRound();
+};
+
 const gameGCD = () => {
   const description = 'Find the greatest common divisor of given numbers.';
-
-  const gameRound = () => {
-    const a = makeRandom();
-    const b = makeRandom();
-    const question = `${a} ${b}`;
-    // Находим число, делящее a и b без остатка.
-    // Перебираем значения от наименьшего из a и b до 1.
-    let i = a > b ? b : a;
-    while (a % i !== 0 || b % i !== 0) i -= 1;
-
-    displayQuestion(question);
-
-    if (checkAnswer(String(i)) === true) gameRound();
-  };
 
   displayIntro(description);
 
