1f93777957ba0fc428b6f2c60abbf30e142f32e6
V. Buko <butkovv1991@gmail.com>
2019-11-23 14:43:00 +0300

    move gameRound function outside of gameProgression function

--- src/games/progressionlib.js
+++ src/games/progressionlib.js
@@ -3,32 +3,32 @@ import {
   displayIntro, displayQuestion, checkAnswer, displayEnding,
 } from '../brainlib';
 
+const gameRound = () => {
+  // Исходное значение
+  let value = makeRandom();
+  // Шаг прогрессии
+  const increment = makeRandom();
+  const progressionLength = 10;
+  // Позиция, которая будет пропущена
+  const missingPosition = makeRandom(progressionLength);
+  let result;
+  let output = '';
+  // Цикл для записи прогрессии в виде строки
+  for (let i = 1; i <= progressionLength; i += 1) {
+    if (i === missingPosition) {
+      output += ' ..';
+      result = value; // Сохраняем пропущенное значение в качестве ответа
+    } else output += ` ${value}`;
+    value += increment;
+  }
+
+  displayQuestion(output);
+
+  if (checkAnswer(String(result)) === true) gameRound();
+};
+
 const gameProgression = () => {
   const description = 'What number is missing in the progression?';
-
-  const gameRound = () => {
-    // Исходное значение
-    let value = makeRandom();
-    // Шаг прогрессии
-    const increment = makeRandom();
-    const progressionLength = 10;
-    // Позиция, которая будет пропущена
-    const missingPosition = makeRandom(progressionLength);
-    let result;
-    let output = '';
-    // Цикл для записи прогрессии в виде строки
-    for (let i = 1; i <= progressionLength; i += 1) {
-      if (i === missingPosition) {
-        output += ' ..';
-        result = value; // Сохраняем пропущенное значение в качестве ответа
-      } else output += ` ${value}`;
-      value += increment;
-    }
-
-    displayQuestion(output);
-
-    if (checkAnswer(String(result)) === true) gameRound();
-  };
 
   displayIntro(description);
 
