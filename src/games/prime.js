5e8e6cbfb35b155f95fa8b45acfd5540f82da0a4
V. Buko <butkovv1991@gmail.com>
2019-11-25 21:49:31 +0300

    fix variables names in games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,22 +6,21 @@ import {
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const gameRound = () => {
-  const upperLimit = 10000; // Задаем максимальное значение генерируемого числа
-  let n = makeRandom(upperLimit); // Генерируем случайное число в заданных пределах
+  const upperLimit = 10000;
+  let number = makeRandom(upperLimit);
   const divisors = [];
   const uniqueDivisors = [];
-  let f = 2; // Начальное значение делителя для алгоритма поиска
+  let div = 2;
 
-  displayQuestion(n);
+  displayQuestion(number);
 
-  // Реализуем алгоритм поиска делителей числа n с добавлением их в массив divisors
-  while (n > 1) {
-    if (n % f === 0) {
-      divisors.push(f);
-      n /= f;
-    } else f += 1;
+  while (number > 1) {
+    if (number % div === 0) {
+      divisors.push(div);
+      number /= div;
+    } else div += 1;
   }
-  // Заполняем массив uniqueDivisors уникальными значениями из массива divisors
+
   for (let i = 0; i < divisors.length; i += 1) {
     if (divisors[i] !== divisors[i - 1]) uniqueDivisors.push(divisors[i]);
   }
