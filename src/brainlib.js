e146c518b5d45e45b0614829a45326dbfcbff81f
V. Buko <butkovv1991@gmail.com>
2019-12-17 21:34:41 +0300

    refactor launchGame function at src/brainlib.js

--- src/brainlib.js
+++ src/brainlib.js
@@ -3,13 +3,12 @@ import { readlineSync, pairs } from './index';
 const roundsCount = 3;
 
 const launchGame = (description, game) => {
-  let winsCount;
   console.log('Welcome to the Brain Games!');
   console.log(description);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 
-  for (winsCount = 0; winsCount < roundsCount; winsCount += 1) {
+  for (let winsCount = 0; winsCount < roundsCount; winsCount += 1) {
     const gameInstance = game();
     const question = pairs.car(gameInstance);
     const correctAnswer = String(pairs.cdr(gameInstance));
@@ -19,13 +18,11 @@ const launchGame = (description, game) => {
       console.log('Correct!');
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-      break;
+      return console.log(`Let's try again, ${userName}!`);
     }
   }
 
-  if (winsCount === roundsCount) console.log(`Congratulations, ${userName}!`);
-  else console.log(`Let's try again, ${userName}!`);
-  return 0;
+  return console.log(`Congratulations, ${userName}!`);
 };
 
 export default launchGame;
